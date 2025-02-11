700ee61cc79ac4a1e5d5c4629d77d4d42bbc2b00
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2025-02-11 15:09:29 +0100

    Creation of some variables

--- webpage-jurap/variables.py
+++ webpage-jurap/variables.py
@@ -5,3 +5,11 @@ current_directory = os.getcwd().replace('\\', '/')
 
 url = f"file:///{current_directory}/webpage-jurap/jurap.html"
 
+#Locators
+REGISTER_BUTTON = "id:register-nav"
+USERNAME_BOX = "id:reg-username"
+PASSWORD_BOX = "id:reg-password"
+
+#Test Data
+USERNAME = "Bookingbot"
+Password = "Finneland12"
